51685fa16f62a0cfad32a51e8bc890c96b56575c
tanixx <127392051+tanixx@users.noreply.github.com>
2023-10-06 15:21:38 +0530

    Update Online.java

--- Online.java
+++ Online.java
@@ -1,6 +1,8 @@
 public class Online{
     public static void main(String args[]){
         System.out.println("Git assignment");
-        System.out.println("branch 1")
+
+        
+        System.out.println("branch 1");
     }
 }
